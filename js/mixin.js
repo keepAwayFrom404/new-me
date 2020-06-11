f609055894cc6d01ff5cba4ca29252513389edd8
keepAwayFrom404 <xff777@qq.com>
2020-06-11 08:06:28 +0800

    feat(mixin): 寄生继承和隐式混入

--- js/mixin.js
+++ js/mixin.js
@@ -1,27 +1,65 @@
-function mixin(source, target) {
-  for(let key in source) {
-    if(!(key in target)) {
-      target[key] = source[key]
-    }
+// function mixin(source, target) {
+//   for(let key in source) {
+//     if(!(key in target)) {
+//       target[key] = source[key]
+//     }
+//   }
+//   return target
+// }
+
+// const Vehicle = {
+//   engines: 1,
+//   ignition: function() {
+//     console.log('Turning on your engine');
+//   },
+//   drive: function() {
+//     this.ignition()
+//     console.log('Steering and moving forword');
+//   }
+// }
+// const Car = mixin(Vehicle, {
+//   wheels: 4,
+//   drive: function() {
+//     Vehicle.drive.call(this)
+//     console.log('Rolling on all ' + this.wheels+' wheel');
+//   }
+// })
+
+// function Vehicle() {
+//   this.engines = 1
+// }
+// Vehicle.prototype.ignition = function() {
+//   console.log('Turning on your engine');
+// }
+// Vehicle.prototype.drive = function() {
+//   this.ignition()
+//   console.log('Steering and moving forword');
+// }
+
+// function Car() {
+//   var car = new Vehicle()
+//   car.wheels = 4
+//   const vehDrive = car.drive
+//   car.drive = function() {
+//     vehDrive.call(this)
+//     console.log('Rolling on all ' + this.wheels+' wheel');
+//   }
+//   return car
+// }
+// const myCar = new Car()
+// myCar.drive()
+
+const something = {
+  cool: function() {
+    this.greeting = 'hello world'
+    this.count = this.count?this.count+1:1
   }
-  return target
 }
 
-const Vehicle = {
-  engines: 1,
-  ignition: function() {
-    console.log('Turning on your engine');
-  },
-  drive: function() {
-    this.ignition()
-    console.log('Steering and moving forword');
+
+const another = {
+  cool: function() {
+    something.cool.call(this)
   }
 }
-const Car = mixin(Vehicle, {
-  wheels: 4,
-  drive: function() {
-    Vehicle.drive.call(this)
-    console.log('Rolling on all ' + this.wheels+' wheel');
-  }
-})
-Car.drive()
+another.cool()
