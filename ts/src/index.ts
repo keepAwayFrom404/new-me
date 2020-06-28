f34cfc2476246e017c36eebeefe9995253206d49
keepAwayFrom404 <xff777@qq.com>
2020-06-28 19:44:00 +0800

    feat(ts): ts learn.

---
+++ ts/src/index.ts
@@ -0,0 +1,22 @@
+// 元组 定长数组
+let tuple: [number, string, boolean] = [1,'22',false]
+
+// 枚举
+enum Roles {
+  admin,
+  super_admin,
+  user
+}
+const superAdmin = Roles.super_admin
+console.log(superAdmin);
+
+// void 没有类型，函数返回值
+const consoleText = (text: string): void => {
+  console.log(text);
+}
+consoleText('hello ts')
+
+const arr: (string[]|number[]) = ['111','123']
+const h1 = document.createElement('h1')
+h1.innerHTML = 'hello TS'
+document.body.appendChild(h1)
