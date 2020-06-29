2327b87aafbdf18c386671fc81d829271e18f43b
keepAwayFrom404 <xff777@qq.com>
2020-06-29 23:12:10 +0800

    feat(ts): enum learn.

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,30 +1,51 @@
-// 元组 定长数组
-let tuple: [number, string, boolean] = [1,'22',false]
+// // 元组 定长数组
+// let tuple: [number, string, boolean] = [1,'22',false]
 
-// 枚举
-enum Roles {
-  admin,
-  super_admin,
-  user
+// // 枚举
+// enum Roles {
+//   admin,
+//   super_admin,
+//   user
+// }
+// const superAdmin = Roles.super_admin
+
+// // void 没有类型，函数返回值
+// const consoleText = (text: string): void => {
+//   console.log(text);
+// }
+// // consoleText('hello ts')
+
+// const arr: (string[]|number[]) = ['111','123']
+// const h1 = document.createElement('h1')
+// h1.innerHTML = 'hello TS'
+// document.body.appendChild(h1)
+
+// const key1: unique symbol = Symbol()
+// let key2: symbol = Symbol('lee')
+// const obj = {
+//   [key1]: 'value1',
+//   [key2]: 'value2',
+// }
+// const start = 1
+// enum Index {
+//   a = start,
+//   b = 2,
+//   c,
+// }
+// const d =  Index.c
+
+enum Status {
+  Success = 200,
+  NotFound = 404,
+  Error = 500
 }
-const superAdmin = Roles.super_admin
-console.log(superAdmin);
 
-// void 没有类型，函数返回值
-const consoleText = (text: string): void => {
-  console.log(text);
+enum E {
+  A,
+  B,
 }
-consoleText('hello ts')
-
-const arr: (string[]|number[]) = ['111','123']
-const h1 = document.createElement('h1')
-h1.innerHTML = 'hello TS'
-document.body.appendChild(h1)
-
-const key1: unique symbol = Symbol()
-let key2: symbol = Symbol('lee')
-const obj = {
-  [key1]: 'value1',
-  [key2]: 'value2',
+const getIndex = (enumObj: {A: number}): number => {
+  return enumObj.A
 }
-console.log(obj[key1]);
+
+console.log(getIndex(E));
