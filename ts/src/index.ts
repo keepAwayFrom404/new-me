35f2928a64163cd06777b1e697ea2949f1ec8f88
keepAwayFrom404 <xff777@qq.com>
2020-06-30 11:40:32 +0800

    feat(ts): interface

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -20,12 +20,20 @@
 // h1.innerHTML = 'hello TS'
 // document.body.appendChild(h1)
 
+
+/**
+ * Symbol
+ */
 // const key1: unique symbol = Symbol()
 // let key2: symbol = Symbol('lee')
 // const obj = {
 //   [key1]: 'value1',
 //   [key2]: 'value2',
 // }
+
+/**
+ * 枚举类型：enum
+ */
 // const start = 1
 // enum Index {
 //   a = start,
@@ -48,11 +56,51 @@
 //   return enumObj.A
 // }
 
+/**
+ * 类型断言
+ */
+// const getLength = (target: number | string): number => {
+//   if((target as string).length) {
+//     return (target as string).length
+//   } else {
+//     return target.toString().length
+//   }
+// }
 
-const getLength = (target: number | string): number => {
-  if((target as string).length) {
-    return (target as string).length
-  } else {
-    return target.toString().length
-  }
+/**
+ * 接口
+ */
+interface Name {
+  firstName: string,
+  lastName: string,
 }
+const getFullName = ({firstName, lastName}: Name) => {
+  console.log(`${firstName} ${lastName}`);
+  return `${firstName} ${lastName}`
+}
+
+interface Vegetables {
+  color?: string,
+  type: string,
+  [prop: string]: any
+}
+const getVegetables = ({ color, type }: Vegetables) => {
+  console.log(`A ${color ? color + " " : ""}${type}`);
+  return `A ${color ? color + " " : ""}${type}`;
+};
+
+getVegetables({type: 'tomoto', size: 'big'})
+
+interface Role {
+  readonly 0: string,
+  readonly 1: string
+}
+
+const role: Role = {
+  0: 'super_admin',
+  1: 'admin'
+}
+
+type AddFunction = (num1: number, num2: number) => number;
+
+const add: AddFunction = (a, b) => a + b
