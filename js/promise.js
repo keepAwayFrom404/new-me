8413e1319d6bed0f2335a3d1479c9b5f8f7717ec
keepAwayFrom404 <xff777@qq.com>
2020-07-19 10:10:20 +0800

    feat(js): promise first

--- js/promise.js
+++ js/promise.js
@@ -174,135 +174,177 @@ const e = require("express")
 /**
  * Promise 手动实现: https://juejin.im/post/5b2f02cd5188252b937548ab
  */
-class CustomPromise {
-  constructor(executor) {
-    this.state = 'pending'
-    this.value = undefined
-    this.reason = undefined
-    this.onResolvedCallbacks = []
-    this.onRejectedCallbacks = []
-    let resolve = (value) => {
-      if(this.state === 'pending') {
-        this.state = 'fulfilled'
-        this.value = value
-        this.onResolvedCallbacks.forEach(fn => fn())
-      }
-    }
-    let reject = (reason) => {
-      if(this.state === 'pending') {
-        this.state = 'rejected'
-        this.reason = reason
-        this.onRejectedCallbacks.forEach(fn => fn())
-      }
-    }
-    try {
-      executor(resolve, reject)
-    } catch (error) {
-      reject(error)
-    }
-  }
-  then(onFulfilled, onRejected) {
-    // onFulfilled如果不是函数，就忽略onFulfilled，直接返回value
-    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
-    // onRejected如果不是函数，就忽略onRejected，直接扔出错误
-    onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err };
-    let promise2 = new Promise((resolve, reject) => {
-      switch (this.state) {
-        case 'fulfilled':
-          setTimeout(() => {
-            try {
-              let x = onFulfilled(this.value)
-              // resolvePromise函数，处理自己return的promise和默认的promise2的关系
-              resolvePromise(promise2, x, resolve, reject);
-            } catch (error) {
-              reject(error)
-            }
-          }, 0);
-          break;
-        case 'rejected':
-          setTimeout(() => {
-            try {
-              let xy = onRejected(this.reason)
-              resolvePromise(promise2, xy, resolve, reject);
-            } catch (error) {
-              reject(error)
-            }
-          }, 0);
-          break;
-        case 'pending':
-          this.onResolvedCallbacks.push(()=>{
-            setTimeout(() => {
-              try {
-                let x = onFulfilled(this.value);
-                resolvePromise(promise2, x, resolve, reject);
-              } catch (error) {
-                reject(error)
-              }
-            }, 0);
-          })
-          this.onRejectedCallbacks.push(()=>{
-            setTimeout(() => {
-              try {
-                let x = onRejected(this.reason);
-                resolvePromise(promise2, x, resolve, reject);
-              } catch (error) {
-                reject(error)
-              }
-            }, 0);
-          })
-          break;
-      }
-    })
-    return promise2
-  }
-}
+// class CustomPromise {
+//   constructor(executor) {
+//     this.state = 'pending'
+//     this.value = undefined
+//     this.reason = undefined
+//     this.onResolvedCallbacks = []
+//     this.onRejectedCallbacks = []
+//     let resolve = (value) => {
+//       if(this.state === 'pending') {
+//         this.state = 'fulfilled'
+//         this.value = value
+//         this.onResolvedCallbacks.forEach(fn => fn())
+//       }
+//     }
+//     let reject = (reason) => {
+//       if(this.state === 'pending') {
+//         this.state = 'rejected'
+//         this.reason = reason
+//         this.onRejectedCallbacks.forEach(fn => fn())
+//       }
+//     }
+//     try {
+//       executor(resolve, reject)
+//     } catch (error) {
+//       reject(error)
+//     }
+//   }
+//   then(onFulfilled, onRejected) {
+//     // onFulfilled如果不是函数，就忽略onFulfilled，直接返回value
+//     onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+//     // onRejected如果不是函数，就忽略onRejected，直接扔出错误
+//     onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err };
+//     let promise2 = new Promise((resolve, reject) => {
+//       switch (this.state) {
+//         case 'fulfilled':
+//           setTimeout(() => {
+//             try {
+//               let x = onFulfilled(this.value)
+//               // resolvePromise函数，处理自己return的promise和默认的promise2的关系
+//               resolvePromise(promise2, x, resolve, reject);
+//             } catch (error) {
+//               reject(error)
+//             }
+//           }, 0);
+//           break;
+//         case 'rejected':
+//           setTimeout(() => {
+//             try {
+//               let xy = onRejected(this.reason)
+//               resolvePromise(promise2, xy, resolve, reject);
+//             } catch (error) {
+//               reject(error)
+//             }
+//           }, 0);
+//           break;
+//         case 'pending':
+//           this.onResolvedCallbacks.push(()=>{
+//             setTimeout(() => {
+//               try {
+//                 let x = onFulfilled(this.value);
+//                 resolvePromise(promise2, x, resolve, reject);
+//               } catch (error) {
+//                 reject(error)
+//               }
+//             }, 0);
+//           })
+//           this.onRejectedCallbacks.push(()=>{
+//             setTimeout(() => {
+//               try {
+//                 let x = onRejected(this.reason);
+//                 resolvePromise(promise2, x, resolve, reject);
+//               } catch (error) {
+//                 reject(error)
+//               }
+//             }, 0);
+//           })
+//           break;
+//       }
+//     })
+//     return promise2
+//   }
+// }
 
-function resolvePromise(promise2, x, resolve, reject) {
-  if(promise2 === x) {
-    return reject(new Error('循环引用错误'))
-  }
-  // 防止多次调用
-  let called
-  // x不是null且x是对象或者函数
-  if(x !== null && (typeof x === 'object' || typeof x === 'function')) {
-    try {
-      let then = x.then
-      // 如果then是函数，就默认是promise了
-      if(typeof then === 'function') {
-        // 就让then执行 第一个参数是this   后面是成功的回调 和 失败的回调
-        then.call(x, y => {
-          if(called) return
-          called = true
-          // resolve的结果依旧是promise 那就继续解析
-          resolvePromise(promise2, y, resolve, reject)
-        }, err => {
-          if(called) return
-          called = true
-          reject(err)
-        })
-      } else {
-        resolve(x)
-      }
-    } catch (error) {
-      if(called) return
-      called = true
-      reject(error)
-    }
-  } else {
-    resolve(x)
-  }
-}
+// function resolvePromise(promise2, x, resolve, reject) {
+//   if(promise2 === x) {
+//     return reject(new Error('循环引用错误'))
+//   }
+//   // 防止多次调用
+//   let called
+//   // x不是null且x是对象或者函数
+//   if(x !== null && (typeof x === 'object' || typeof x === 'function')) {
+//     try {
+//       let then = x.then
+//       // 如果then是函数，就默认是promise了
+//       if(typeof then === 'function') {
+//         // 就让then执行 第一个参数是this   后面是成功的回调 和 失败的回调
+//         then.call(x, y => {
+//           if(called) return
+//           called = true
+//           // resolve的结果依旧是promise 那就继续解析
+//           resolvePromise(promise2, y, resolve, reject)
+//         }, err => {
+//           if(called) return
+//           called = true
+//           reject(err)
+//         })
+//       } else {
+//         resolve(x)
+//       }
+//     } catch (error) {
+//       if(called) return
+//       called = true
+//       reject(error)
+//     }
+//   } else {
+//     resolve(x)
+//   }
+// }
 
-const cp = new CustomPromise((res, rej) => {
-  setTimeout(() => {
-    res(1342222)
-  }, 1000);
-})
-cp.then((res) => {
-  console.log(res);
-  return 123
-}, (rej) => {
-  console.log(rej, 1111);
-}).then(re => {
-  console.log(re);
-})
+// const cp = new CustomPromise((res, rej) => {
+//   setTimeout(() => {
+//     res(1342222)
+//   }, 1000);
+// })
+// cp.then((res) => {
+//   console.log(res);
+//   return 123
+// }, (rej) => {
+//   console.log(rej, 1111);
+// }).then(re => {
+//   console.log(re);
+// })
+
+/**
+ * Promise first 
+ */
+
+ if(!Promise.first) {
+   Promise.first = function(prs) {
+     return new Promise((resolve, reject) => {
+       const rejArr =[]
+       prs.forEach(element => {
+         Promise.resolve(element)
+         .then(resolve)
+         .catch(err => {
+           rejArr.push(err)
+           if(prs.length === rejArr.length) reject(rejArr[0])
+         })
+       });
+     })
+   }
+ }
+
+ const Pms1 = new Promise((res, rej) => {
+   setTimeout(() => {
+     rej(1000)
+   }, 1000);
+ })
+ const Pms2 = new Promise((res, rej) => {
+   setTimeout(() => {
+    rej(2000)
+   }, 2000);
+ })
+ const Pms3 = new Promise((res, rej) => {
+   setTimeout(() => {
+     rej(1500)
+   }, 1500);
+ })
+
+ Promise.first([Pms1, Pms2, Pms3]).then(res => {
+   console.log(res);
+ }).catch(err => {
+   console.log(err);
+ })
