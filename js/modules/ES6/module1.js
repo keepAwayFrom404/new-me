d0110ac59a3eb2a767d0e013392904841d196fab
keepAwayFrom404 <xff777@qq.com>
2020-07-26 11:58:05 +0800

    feat(js): es6 module.

---
+++ js/modules/ES6/module1.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.foo = foo;
+exports.bar = bar;
+function foo() {
+  console.log('i am module1 foo()');
+}
+function bar() {
+  console.log('i am module1 bar()');
+}
---
+++ js/modules/ES6/module2.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+function func1() {
+  console.log('i am module2 func1()');
+}
+function func2() {
+  console.log('i am module2 func2()');
+}
+exports.func1 = func1;
+exports.func2 = func2;
