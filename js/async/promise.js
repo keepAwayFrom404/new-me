f72349aad46686f1fadb0b0e8c207c7839797f80
keepAwayFrom404 <xff777@qq.com>
2020-10-15 08:54:19 +0800

    docs: promise arguments.

--- js/async/promise.js
+++ js/async/promise.js
@@ -1,3 +1,4 @@
+// 传递给Promise的参数应该是resolve和reject因为resolve是完成之意（可能成功也可能失败类似于Promise.resolve()对于传入的Promise或者thenable会展开，而reject不会）；传递给then的参数应该为fulfilled和rejected更加准确。
 const fetchX = 1
 let fetchY = Promise.resolve(2).then(res => {
   setTimeout(() => {
