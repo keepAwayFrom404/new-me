d942146432f0d34f125c520ea614147588787974
keepAwayFrom404 <xff777@qq.com>
2021-04-01 14:45:34 +0800

    feat(webpack): 生产环境eslint与babel配置

--- webpack/02.生产环境配置/src/index.js
+++ webpack/02.生产环境配置/src/index.js
@@ -1,2 +1,18 @@
-import './styles/a.css'
-import './styles/b.css'
+import 'core-js/modules/es.promise.js';
+import 'core-js/modules/es.object.to-string.js';
+import 'core-js/modules/web.timers.js';
+
+import './styles/a.css';
+import './styles/b.css';
+
+const add = function add(x, y) {
+  return x + y;
+};
+
+console.log(add(2, 5));
+const p = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve('done~');
+  }, 1000);
+});
+p.then((res) => console.log(res));
--- webpack/02.生产环境配置/webpack.config.js
+++ webpack/02.生产环境配置/webpack.config.js
@@ -1,7 +1,7 @@
-const { resolve } = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
 // process.env.NODE_ENV = 'development'
 
 module.exports = {
@@ -16,28 +16,69 @@ module.exports = {
         test: /\.css$/,
         use: [
           // 'style-loader',
+          // 将css从js中剥离
           MiniCssExtractPlugin.loader,
           'css-loader',
           {
             loader: 'postcss-loader',
             options: {
               postcssOptions: {
-                plugins: ['postcss-preset-env']
-              }
-            }
-          }
-        ]
+                plugins: ['postcss-preset-env'],
+              },
+            },
+          },
+        ],
       },
-    ]
+      {
+        test: /\.js$/,
+        include: /webpack/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+        },
+      },
+      {
+        // js兼容性：babel-loader，进行兼容性处理与语法转换
+        loader: 'babel-loader',
+        options: {
+          // 预设：指示babel做什么兼容性处理，@babel/preset-env只能转换基本语法
+          // @babel/polyfill: 一次性引入，体积太大
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                useBuiltIns: 'usage',
+                corejs: {
+                  version: '3.10',
+                },
+                // 具体兼容性到
+                targets: {
+                  chrome: '60',
+                  firefox: '60',
+                  ie: '9',
+                  safari: '10',
+                  edge: '17',
+                },
+              },
+            ],
+          ],
+        },
+        exclude: /node_modules/,
+        include: /webpack/,
+        test: /\.js$/,
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
     }),
+    // 压缩css
     new MiniCssExtractPlugin({
-      filename: 'css/built.css'
+      filename: 'css/built.css',
     }),
-    new OptimizeCssAssetsWebpackPlugin()
+    new OptimizeCssAssetsWebpackPlugin(),
   ],
   mode: 'development',
   // mode: 'production',
@@ -45,6 +86,6 @@ module.exports = {
     contentBase: resolve(__dirname, 'public'),
     compress: true,
     port: 3000,
-    open: true
-  }
-}
+    open: true,
+  },
+};
