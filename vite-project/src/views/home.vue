1d4dc9f956fa4d31b3edfb693beefce81f7e8b6d
keepAwayFrom404 <xff777@qq.com>
2021-06-26 09:49:55 +0800

    feat(vite): 搭建vite全家桶。

---
+++ vite-project/src/views/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>i am home</div>
+    <div>{{$store.state.count}}</div>
+    <button @click="$store.commit('increment')">add</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
